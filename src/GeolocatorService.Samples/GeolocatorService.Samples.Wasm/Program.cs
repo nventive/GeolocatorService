08bc52d02ff0d2a2ae47c10ed59da209504fd1df
Ryan Wintle <ryanwin98@outlook.com>
2022-09-26 13:18:38 -0400

    feat: replacement of windows.ui to microsoft.ui

--- src/GeolocatorService.Samples/GeolocatorService.Samples.Wasm/Program.cs
+++ src/GeolocatorService.Samples/GeolocatorService.Samples.Wasm/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Windows.UI.Xaml;
+using Microsoft.UI.Xaml;
 
 namespace GeolocatorService.Samples.Wasm
 {
@@ -9,7 +9,7 @@ namespace GeolocatorService.Samples.Wasm
 
 		static int Main(string[] args)
 		{
-			Windows.UI.Xaml.Application.Start(_ => _app = new App());
+			Microsoft.UI.Xaml.Application.Start(_ => _app = new App());
 
 			return 0;
 		}
