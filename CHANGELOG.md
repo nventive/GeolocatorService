08adb7a8500a2139b615c39f651fc71f14167bee
Carl Brunet <carl.brunet@nventive.com>
2023-03-21 13:20:57 -0400

    refactor: Added Abstractions project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [Unreleased]
 
 ### Added
+* [2023-03-23] Added Abstractions Project
 * [2022-08-24] Add support for Android 12
 
 ### Changed
---
+++ src/GeolocatorService.Abstractions/Geocoordinate/GeoshapeType.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeolocatorService
+{
+	public enum GeoshapeType
+	{
+		/// <summary>
+		/// The geographic region is a point.
+		/// </summary>
+		Geopoint,
+		/// <summary>
+		/// The geographic region is a circle with a center point and a radius.
+		/// </summary>
+		Geocircle,
+		/// <summary>
+		/// The geographic region is an order series of points.
+		/// </summary>
+		Geopath,
+		/// <summary>
+		/// The geographic region is a rectangular region.
+		/// </summary>
+		GeoboundingBox,
+	}
+}
