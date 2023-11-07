315de225ef04cddf6af5889d0d89d14da5461eac
Laurent Proulx <laurent.proulx@nventive.com>
2023-11-07 15:12:26 -0500

    feat: Windows workingupdate head for uno 5, add windows project

---
+++ src/GeolocatorServiceSamples/GeolocatorServiceSamples/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace GeolocatorServiceSamples
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
